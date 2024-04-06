c4899464c445e3fc6bd6f2ee6991543bc18febd1
wahome <wahomewgitonga@gmail.com>
2024-04-06 09:31:27 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+const yourName = 'Your Name';
+newHeader.textContent = `${yourName} is the champion`;
+
+
+document.body.appendChild(newHeader);
+
